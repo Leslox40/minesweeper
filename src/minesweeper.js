0335e55e309f71bbca14302164ed03f92f71303b
Leslox40 <fonepiles40x@gmail.com>
2018-08-02 17:19:56 +0100

    Done with the hardcode of the minesweeper game for terminal display.

---
+++ src/minesweeper.js
@@ -0,0 +1,16 @@
+const blankLine = '  |   |  ';
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+//creating variable for guess play
+const guessLine = '1 |   |  ';
+//mine display variable
+const bombLine = '  | B |  ';
+
+console.log('This is what a board with a guess and a bomb on it wouldlook like:');
+
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);

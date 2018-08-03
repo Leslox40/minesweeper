72df68dde795cee8b910620434e941f96f4d5e6d
Leslox40 <fonepiles40x@gmail.com>
2018-08-03 16:01:35 +0100

    Done with dynamically generating the board with arrays and functions. Still hard coding though!

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,16 +1,13 @@
-const blankLine = '  |   |  ';
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+const printBoard = (board) => {
+  console.log('Current Board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+}
+let board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+ ];
 
-//creating variable for guess play
-const guessLine = '1 |   |  ';
-//mine display variable
-const bombLine = '  | B |  ';
-
-console.log('This is what a board with a guess and a bomb on it wouldlook like:');
-
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+printBoard(board);

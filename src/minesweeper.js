ac5c53ff0e6567049025d014dd98b88e6d714c4b
Leslox40 <fonepiles40x@gmail.com>
2018-08-14 21:17:22 +0100

    Finished function to dynamically generate player's board no matter the number of rows and columns

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,13 +1,17 @@
-const printBoard = (board) => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
-}
-let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
- ];
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = []
 
-printBoard(board);
+  for(rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    //making an empty array that will server as a row for the board
+    let row = []
+
+//looping through the columns to add the rows
+    for(columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+    //push an empty space that acts as a square on the board
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+}
+console.log(generatePlayerBoard(5, 4));
